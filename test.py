165dde36773cfc3f9ba089204103c58d514c01f5
bilgehannal <bilgehanl.03@gmail.com>
2019-12-09 00:35:03 +0300

    changed unit test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import message
 
 def test_message():
     warning = "Message should be 'Hello World'"
-    assert message.get_message() == 'Hello World', warning
+    assert "Hello" in message.get_message(), warning
 
 
 if __name__ == "__main__":
